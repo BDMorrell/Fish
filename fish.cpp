c1fe73b3b7b23c3fac0393fcbff65b6cea91cef1
BDMorrell <BDMorrell@gmail.com>
2015-06-26 16:36:21 -0600

    Added testing functions and it compiles

--- fish.cpp
+++ fish.cpp
@@ -9,7 +9,7 @@ public:
 	fish(int breed) {
 		if (fish::nextIdFactory == 0) fish::nextIdFactory = new int(0); //make id factory value
 		id = (*fish::nextIdFactory)++;
-		type = breed; 
+		type = breed;
 	}
 	int type;
 	int getId() {return id;}
@@ -20,9 +20,10 @@ int * fish::nextIdFactory = 0;
 
 class simData {
 public:
-	simData(int numOfType, int *numInType) { //argc, argv for numbers
+	simData(int numOfType, int *numInType, int numOfImportant, int *Important) { //argc, argv for numbers
 		int i, j; //count fish to make
 		totalFish = 0;
+		typesOfFish = numOfType;
 		for (i = 0; i < numOfType; i++)
 			totalFish += numInType[i];
 		allFish = new fish*[totalFish];
@@ -44,18 +45,39 @@ public:
 		for (i = 0; i < totalFish; i++)
 			pond.push_front(allFish[i]);
 	}
+	int *getLivingFishTypes() { //total amounts of living fish //destroy return value when done
+		int *types = new int[typesOfFish];
+		list<fish*>::iterator it;
+		for (it = pond.begin(); it != pond.end(); it++)
+			types[(*it)->type]++;
+		return types;
+	}
+	bool simulationFinished() { //if an important fish type is extinct
+		int *livingBreeds = getLivingFishTypes(); //delete returned value
+		int i;
+		bool finished = false; //value to return
+		for (i = 0; i < numImportantFishTypes && !finished; i++)
+			if (livingBreeds[importantFishTypes[i]] == 0)
+				finished = true;
+		delete[] livingBreeds;
+		return finished;
+	}
+	int getTypesOfFish() {return typesOfFish;}
 	int getTotalFish() {return totalFish;}
 	fish **allFish; // * of array, * to element in array
 	list<fish*> pond; // "living" fish
+	int numImportantFishTypes; //size of importantFishTypes
+	int *importantFishTypes; //array of who's important
 private:
+	int typesOfFish; //for totaling the ammounts of living fish
 	int totalFish; //so we can delete the fish when we are done
 };
 
 /*
-TODO:
+todo:
 Run the simulation (peudo code below):
 funciton test():
-	while (havn't reun through all the fish)
+	while (havn't run through all the fish)
 		take next fish
 		if (!done) //Dan or all trout died
 			test()
@@ -64,10 +86,9 @@ funciton test():
 */
 int main()
 {
-	int fishType[] {5, 9, 1}; //trout catfish Dan
-	simData simulation((sizeof(fishType)/sizeof(int)), fishType); //update the line above to change the array
-	
+	int fishType[] = {5, 9, 1}; //trout catfish Dan
+	int importantFishTypes[] = {0, 2}; //trout and Dan
+	simData simulation((sizeof(fishType)/sizeof(int)), fishType, (sizeof(importantFishTypes)/sizeof(int)), importantFishTypes); //update the lines above to change the arrays
 	
 	return 0;
 }
-
